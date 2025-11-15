b211ce5cf20cc950b56e0550d334ef9238410ab0
king_abdul <134756071+brianwibowo@users.noreply.github.com>
2025-11-16 01:15:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -924,6 +924,3 @@ def laporan_page():
         buku_besar=buku_besar_data
     )
 
-# ---------------- Menjalankan Aplikasi (LOKAL) ----------------
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
